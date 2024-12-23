f7a80ef5cdb171662da9816256bf37407108c825
SajeedAninda <sajaninda@gmail.com>
2024-12-23 18:06:19 +0600

    Added transition effects in navbar hover

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Homepage from "./Components/Home/Homepage"
 import Navbar from "./Components/Navbar/Navbar"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Homepage></Homepage>
     </>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,24 +5,30 @@ import cartIcon from '../../assets/images/shared/desktop/icon-cart.svg'
 const Navbar = () => {
   return (
     <div className='bg-[#181918]'>
-      <div className='w-[100%] lg:w-[1130px] mx-auto h-[14vh] border-b border-gray-400 flex justify-between items-center'>
+      <div className='w-[100%] lg:w-[1130px] mx-auto h-[14vh] border-b border-[#3a3a3a] flex justify-between items-center'>
         <div className='logo'>
           <img src={logo} alt='' />
         </div>
 
         <div className='links flex gap-10 items-center text-[15px] text-white font-semibold uppercase tracking-widest'>
-          <p className='hover:text-[#d87d4a] cursor-pointer'>Home</p>
-          <p className='hover:text-[#d87d4a] cursor-pointer'>Headphones</p>
-          <p className='hover:text-[#d87d4a] cursor-pointer'>Speakers</p>
-          <p className='hover:text-[#d87d4a] cursor-pointer'>Earphones</p>
+          <p className='hover:text-[#d87d4a] cursor-pointer transition-all duration-300 delay-75'>
+            Home
+          </p>
+          <p className='hover:text-[#d87d4a] cursor-pointer transition-all duration-300 delay-75'>
+            Headphones
+          </p>
+          <p className='hover:text-[#d87d4a] cursor-pointer transition-all duration-300 delay-75'>
+            Speakers
+          </p>
+          <p className='hover:text-[#d87d4a] cursor-pointer transition-all duration-300 delay-75'>
+            Earphones
+          </p>
         </div>
 
-        <div className="cart">
-            <img className='cursor-pointer' src={cartIcon} alt="" />
+        <div className='cart'>
+          <img className='cursor-pointer' src={cartIcon} alt='' />
         </div>
       </div>
-
-
     </div>
   )
 }
