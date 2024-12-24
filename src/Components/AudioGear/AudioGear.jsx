885c97f872141fd662c9924d91786e67197a1e4f
SajeedAninda <sajaninda@gmail.com>
2024-12-24 18:15:33 +0600

    Completed audio gear section

--- src/Components/AudioGear/AudioGear.jsx
+++ src/Components/AudioGear/AudioGear.jsx
@@ -1,25 +1,30 @@
 import React from 'react'
-import yx1EarphoneImg from '../../assets/images/home/desktop/image-earphones-yx1.jpg'
-
+import audioGearImg from '../../assets/images/shared/desktop/image-best-gear.jpg'
 
 const AudioGear = () => {
   return (
     <div className='w-[100%] lg:w-[1130px] mx-auto mb-32 flex gap-8 justify-between items-center'>
-      <div className='div rounded-xl w-[50%]'>
-        <img
-          className='rounded-xl object-contain w-full'
-          src={yx1EarphoneImg}
-          alt=''
-        />
+      <div className='w-[50%]'>
+        <h1 className='uppercase text-[40px] leading-[50px] text-[#191919] font-bold'>
+          Bringing you the <span className='text-[#d87d4a]'>best</span> audio
+          gear
+        </h1>
+        <p className='text-[15px] text-[#00000080] mt-8 w-[90%]'>
+          Located at the heart of New York City, Audiophile is the premier store
+          for high end headphones, earphones, speakers, and audio accessories.
+          We have a large showroom and luxury demonstration rooms available for
+          you to browse and experience a wide range of our products. Stop by our
+          store to meet some of the fantastic people who make Audiophile the
+          best place to buy your portable audio equipment.
+        </p>
       </div>
 
-      <div className='w-[50%] rounded-xl py-24 pl-24 bg-[#f1f1f1]'>
-        <h1 className='uppercase text-[28px] text-[#191919] font-bold'>
-          YX1 Earphone
-        </h1>
-        <button className='bg-transparent px-10 py-3 hover:bg-[#191919] text-[#191919] hover:text-white border border-[#191919] font-bold uppercase text-[13px] mt-8 transition-colors duration-300 ease-in-out'>
-          See Product
-        </button>
+      <div className='div rounded-xl w-[50%]'>
+        <img
+          className='rounded-xl object-cover w-full'
+          src={audioGearImg}
+          alt=''
+        />
       </div>
     </div>
   )
