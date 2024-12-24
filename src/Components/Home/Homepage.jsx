ad1d14d0d3a456dc0e96339dfec8f597a47002b2
SajeedAninda <sajaninda@gmail.com>
2024-12-24 18:02:42 +0600

    Created yx1 earphone section in homepage

--- src/Components/Home/Homepage.jsx
+++ src/Components/Home/Homepage.jsx
@@ -3,6 +3,7 @@ import Hero from './Hero/Hero';
 import Shop from './Shop/Shop';
 import ZX9_Speaker from '../ZX9-Speaker/ZX9_Speaker';
 import ZX7_Speaker from '../ZX7-Speaker/ZX7_Speaker';
+import YX1_Eaphone from '../YX1-Earphones/YX1_Eaphone';
 
 const Homepage = () => {
     return (
@@ -11,6 +12,7 @@ const Homepage = () => {
            <Shop></Shop>
            <ZX9_Speaker></ZX9_Speaker>
            <ZX7_Speaker></ZX7_Speaker>
+           <YX1_Eaphone></YX1_Eaphone>
         </>
     );
 };
---
+++ src/Components/YX1-Earphones/YX1_Eaphone.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import yx1EarphoneImg from '../../assets/images/home/desktop/image-earphones-yx1.jpg'
+
+const YX1_Eaphone = () => {
+  return (
+    <div className='w-[100%] lg:w-[1130px] mx-auto mb-20 flex gap-8 justify-between items-center'>
+      <div className='div rounded-xl w-[50%]'>
+        <img
+          className='rounded-xl object-contain w-full'
+          src={yx1EarphoneImg}
+          alt=''
+        />
+      </div>
+
+      <div className='w-[50%] rounded-xl py-24 pl-24 bg-[#f1f1f1]'>
+        <h1 className='uppercase text-[28px] text-[#191919] font-bold'>
+          YX1 Earphone
+        </h1>
+        <button className='bg-transparent px-10 py-3 hover:bg-[#191919] text-[#191919] hover:text-white border border-[#191919] font-bold uppercase text-[13px] mt-8 transition-colors duration-300 ease-in-out'>
+          See Product
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default YX1_Eaphone
