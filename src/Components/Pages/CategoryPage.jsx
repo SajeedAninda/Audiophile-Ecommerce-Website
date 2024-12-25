283ca07727fdedfd3dd18709effb7bd47029bc1f
SajeedAninda <sajaninda@gmail.com>
2024-12-25 17:14:06 +0600

    Received the route name in component

--- src/Components/Pages/CategoryPage.jsx
+++ src/Components/Pages/CategoryPage.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 const CategoryPage = () => {
-    return (
-        <div>
-            New Category
-        </div>
-    );
-};
+  let location = useLocation()
+  let routeName = location.pathname.slice(1)
+  console.log(routeName)
 
-export default CategoryPage;
+  return <div>New Category</div>
+}
+
+export default CategoryPage
