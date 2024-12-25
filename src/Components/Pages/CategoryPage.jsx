94b202cbb2ac35dfee9c0e663a02f96ad7237a5e
SajeedAninda <sajaninda@gmail.com>
2024-12-25 17:35:20 +0600

    Fetched Data based on route name

--- src/Components/Pages/CategoryPage.jsx
+++ src/Components/Pages/CategoryPage.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 
 const CategoryPage = () => {
-  let location = useLocation()
-  let routeName = location.pathname.slice(1)
-  console.log(routeName)
+  const location = useLocation()
+  const routeName = location.pathname.slice(1)
+  let [categoryData, setCategoryData] = useState([])
 
-  return <div>New Category</div>
+  useEffect(() => {
+    fetch('/products.json')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch data')
+        }
+        return response.json()
+      })
+      .then(data => {
+        const matchedData = data.products.filter(
+          product => product.category === routeName
+        )
+        setCategoryData(matchedData)
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error)
+      })
+  }, [routeName])
+
+  console.log(categoryData)
+
+  return <div>Fetching data for category: {routeName}</div>
 }
 
 export default CategoryPage
