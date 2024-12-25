e075803919bd79eb354adfda5f191ec2137831ce
SajeedAninda <sajaninda@gmail.com>
2024-12-25 17:47:55 +0600

    Created a framework of product

--- src/Components/Pages/CategoryPage.jsx
+++ src/Components/Pages/CategoryPage.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
+import headphoneImg from '../../assets/images/product-xx99-mark-two-headphones/desktop/image-product.jpg'
 
 const CategoryPage = () => {
   const location = useLocation()
   const routeName = location.pathname.slice(1)
-  let [categoryData, setCategoryData] = useState([])
+  const [categoryData, setCategoryData] = useState([])
 
   useEffect(() => {
     fetch('/products.json')
@@ -27,7 +28,31 @@ const CategoryPage = () => {
 
   console.log(categoryData)
 
-  return <div>Fetching data for category: {routeName}</div>
+  return (
+    <div>
+      <div className='bg-[#181918]'>
+        <div className='w-[100%] lg:w-[1130px] mx-auto text-center py-24'>
+          <h1 className='text-white text-[40px] font-bold uppercase'>
+            {routeName}
+          </h1>
+        </div>
+      </div>
+
+      <div className='h-fit'>
+        <div lassName='w-[100%] lg:w-[1130px] mx-auto gap-8 flex justify-between items-center'>
+          <div className='imgDiv flex-1'>
+            <img src={headphoneImg} alt="" />
+          </div>
+
+          <div className='textDiv flex-1'>
+            <p className=''>
+                New Product
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default CategoryPage
