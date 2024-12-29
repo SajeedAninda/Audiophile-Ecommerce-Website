2b8a32332c3e4ca0b1ea494b233c27cad7c269c5
SajeedAninda <sajaninda@gmail.com>
2024-12-29 19:01:55 +0600

    Added scroll to top feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { Outlet } from 'react-router-dom'
 import Footer from './Components/Footer/Footer'
 import Homepage from './Components/Home/Homepage'
 import Navbar from './Components/Navbar/Navbar'
+import ScrollToTop from './Components/ScrollToTop/ScrollToTop'
 
 function App () {
   return (
     <>
+      <ScrollToTop></ScrollToTop>
       <Navbar></Navbar>
       <Outlet></Outlet>
       <Footer></Footer>
