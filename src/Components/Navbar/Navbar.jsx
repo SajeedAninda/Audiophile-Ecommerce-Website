87e59dbf19ac89e1a373f290cb41347ea263f9cc
SajeedAninda <sajaninda@gmail.com>
2024-12-30 17:48:28 +0600

    Added active link styles in navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,49 +1,65 @@
-import React from 'react'
-import logo from '../../assets/images/shared/desktop/logo.svg'
-import cartIcon from '../../assets/images/shared/desktop/icon-cart.svg'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import logo from '../../assets/images/shared/desktop/logo.svg';
+import cartIcon from '../../assets/images/shared/desktop/icon-cart.svg';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className='bg-[#181918]'>
       <div className='w-[100%] lg:w-[1130px] mx-auto h-[14vh] border-b border-[#3a3a3a] flex justify-between items-center'>
         <div className='logo'>
-          <img src={logo} alt='' />
+          <img src={logo} alt='Logo' />
         </div>
 
         <div className='links flex gap-10 items-center text-[15px] text-white font-semibold uppercase tracking-widest'>
           <NavLink
             to={'/'}
-            className='hover:text-[#d87d4a] cursor-pointer transition-all duration-300 delay-75'
+            className={({ isActive }) =>
+              `cursor-pointer transition-all duration-300 delay-75 hover:text-[#d87d4a] ${
+                isActive ? 'text-[#d87d4a]' : 'text-white'
+              }`
+            }
           >
             Home
           </NavLink>
           <NavLink
             to={'/headphones'}
-            className='hover:text-[#d87d4a] cursor-pointer transition-all duration-300 delay-75'
+            className={({ isActive }) =>
+              `cursor-pointer transition-all duration-300 delay-75 hover:text-[#d87d4a] ${
+                isActive ? 'text-[#d87d4a]' : 'text-white'
+              }`
+            }
           >
             Headphones
           </NavLink>
           <NavLink
             to={'/speakers'}
-            className='hover:text-[#d87d4a] cursor-pointer transition-all duration-300 delay-75'
+            className={({ isActive }) =>
+              `cursor-pointer transition-all duration-300 delay-75 hover:text-[#d87d4a] ${
+                isActive ? 'text-[#d87d4a]' : 'text-white'
+              }`
+            }
           >
             Speakers
           </NavLink>
           <NavLink
             to={'/earphones'}
-            className='hover:text-[#d87d4a] cursor-pointer transition-all duration-300 delay-75'
+            className={({ isActive }) =>
+              `cursor-pointer transition-all duration-300 delay-75 hover:text-[#d87d4a] ${
+                isActive ? 'text-[#d87d4a]' : 'text-white'
+              }`
+            }
           >
             Earphones
           </NavLink>
         </div>
 
         <div className='cart'>
-          <img className='cursor-pointer' src={cartIcon} alt='' />
+          <img className='cursor-pointer' src={cartIcon} alt='Cart Icon' />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
