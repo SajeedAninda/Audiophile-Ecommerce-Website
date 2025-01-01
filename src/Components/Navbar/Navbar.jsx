b4e34f91253eea395a6a7b87e4dc295ceeddd1fa
SajeedAninda <sajaninda@gmail.com>
2025-01-01 16:43:30 +0600

    Called the modal from navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import logo from '../../assets/images/shared/desktop/logo.svg';
-import cartIcon from '../../assets/images/shared/desktop/icon-cart.svg';
-import { NavLink } from 'react-router-dom';
+import React, { useContext, useState } from 'react'
+import logo from '../../assets/images/shared/desktop/logo.svg'
+import cartIcon from '../../assets/images/shared/desktop/icon-cart.svg'
+import { NavLink } from 'react-router-dom'
+import { CartContext } from '../ContextAPI/CartProvider'
+import Modal from '../Modal/Modal'
 
 const Navbar = () => {
+  const [isModalOpen, setModalOpen] = useState(false)
+  const { cartItems } = useContext(CartContext)
+
+  const toggleModal = () => setModalOpen(!isModalOpen)
+
   return (
     <div className='bg-[#181918]'>
       <div className='w-[100%] lg:w-[1130px] mx-auto h-[14vh] border-b border-[#3a3a3a] flex justify-between items-center'>
@@ -54,12 +61,14 @@ const Navbar = () => {
           </NavLink>
         </div>
 
-        <div className='cart'>
+        <div className='cart' onClick={toggleModal}>
           <img className='cursor-pointer' src={cartIcon} alt='Cart Icon' />
         </div>
+
+        {isModalOpen && <Modal cartItems={cartItems} onClose={toggleModal} />}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
