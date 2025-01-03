533d0c0ad9d787cfbd14609a2dd31db0ce94423e
SajeedAninda <sajaninda@gmail.com>
2025-01-03 19:14:41 +0600

    Modal closes when clicked outside

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,40 +1,36 @@
-import React from "react";
+import React from 'react'
 
 const Modal = ({ cartItems, onClose }) => {
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+    <div
+      className='fixed inset-0 z-[9999] bg-black bg-opacity-50 flex justify-center items-center'
+      onClick={onClose}
+    >
       <div
-        className="bg-white rounded-lg p-5 w-[90%] lg:w-[500px]"
-        onClick={(e) => e.stopPropagation()}
+        className='bg-white rounded-lg px-8 py-6 w-[90%] lg:w-[500px]'
+        onClick={e => e.stopPropagation()}
       >
-        <button
-          onClick={onClose}
-          className="text-black bg-gray-200 px-3 py-1 rounded mb-4"
-          aria-label="Close modal"
-        >
-          Close
-        </button>
-        <h2 className="text-lg font-bold mb-4">Cart</h2>
+        <h2 className='text-lg font-bold mb-4'>Cart</h2>
         {cartItems.length === 0 ? (
           <p>Your cart is empty.</p>
         ) : (
           <ul>
-            {cartItems.map((item) => (
+            {cartItems.map(item => (
               <li
                 key={item.slug}
-                className="flex justify-between items-center mb-4"
+                className='flex justify-between items-center mb-4'
               >
                 <div>
                   <img
                     src={item.image}
                     alt={item.name}
-                    className="w-16 h-16 object-cover rounded"
+                    className='w-16 h-16 object-cover rounded'
                   />
                 </div>
                 <div>
-                  <p className="font-bold">{item.name}</p>
+                  <p className='font-bold'>{item.name}</p>
                   <p>Quantity: {item.quantity}</p>
-                  <p>Price: ${ (item.price * item.quantity).toFixed(2) }</p>
+                  <p>Price: ${(item.price * item.quantity).toFixed(2)}</p>
                 </div>
               </li>
             ))}
@@ -42,7 +38,7 @@ const Modal = ({ cartItems, onClose }) => {
         )}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Modal;
+export default Modal
