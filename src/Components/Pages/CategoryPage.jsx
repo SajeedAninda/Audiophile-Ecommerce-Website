523c4de28389a9705c85bda480be6a02a558894c
SajeedAninda <sajaninda@gmail.com>
2025-01-08 16:40:30 +0600

    Made

--- src/Components/Pages/CategoryPage.jsx
+++ src/Components/Pages/CategoryPage.jsx
@@ -45,11 +45,11 @@ const CategoryPage = () => {
             return (
               <div
                 key={product?.id}
-                className={`product flex ${
-                  isRowReversed ? 'flex-row-reverse' : 'flex-row'
+                className={`product flex flex-col px-6 lg:px-0 ${
+                  isRowReversed ? 'lg:flex-row-reverse' : 'lg:flex-row'
                 } gap-10 justify-between items-center mb-28`}
               >
-                <div className='imgDiv w-[50%]'>
+                <div className='imgDiv w-full lg:w-[50%]'>
                   <img
                     className='rounded-xl'
                     src={product?.image?.desktop}
@@ -58,22 +58,22 @@ const CategoryPage = () => {
                 </div>
 
                 <div
-                  className={`textDiv w-[50%] ${isRowReversed ? '' : 'pl-20'}`}
+                  className={`textDiv w-full lg:w-[50%] ${isRowReversed ? '' : 'pl-0 lg:pl-20'}`}
                 >
                   {product?.new && (
-                    <p className='text-[14px] text-[#d87d4a] uppercase tracking-[10px]'>
+                    <p className='text-[14px] text-[#d87d4a] uppercase tracking-[10px] text-center lg:text-left'>
                       New Product
                     </p>
                   )}
-                  <h3 className='text-[#191919] text-[30px] font-bold uppercase mt-6 leading-[35px] w-[50%]'>
+                  <h3 className='text-[#191919] text-[30px] font-bold uppercase mt-6 leading-[35px] w-full lg:w-[50%] text-center lg:text-left'>
                     {product?.name}
                   </h3>
 
-                  <p className='mt-6 text-[15px] text-[#00000080] leading-[30px] w-[90%]'>
+                  <p className='mt-6 text-[15px] text-[#00000080] leading-[30px] w-full lg:w-[90%] text-center lg:text-left'>
                     {product?.description}
                   </p>
 
-                  <div className='mt-6'>
+                  <div className='mt-6 flex justify-center lg:justify-start'>
                     <Link
                       to={`/${product?.slug}`}
                       className='bg-[#d87d4a] px-10 py-3 hover:bg-[#db956c] text-white font-bold uppercase text-[13px] mt-6 transition-colors duration-300 ease-in-out'
@@ -88,10 +88,10 @@ const CategoryPage = () => {
         </div>
       </div>
 
-      <div className='-mt-20'>
+      <div className='-mt-40 lg:-mt-20'>
         <Shop></Shop>
       </div>
-      <div>
+      <div className='mt-[540px] lg:mt-0'>
         <AudioGear></AudioGear>
       </div>
     </div>
