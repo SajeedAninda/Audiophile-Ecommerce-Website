2d675ed1742dfdda15337db2d44055d471d16145
SajeedAninda <sajaninda@gmail.com>
2025-01-08 17:18:10 +0600

    Responsive styling for navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import cartIcon from '../../assets/images/shared/desktop/icon-cart.svg'
 import { NavLink } from 'react-router-dom'
 import { CartContext } from '../ContextAPI/CartProvider'
 import Modal from '../Modal/Modal'
+import { GiHamburgerMenu } from 'react-icons/gi'
 
 const Navbar = () => {
   const [isModalOpen, setModalOpen] = useState(false)
@@ -17,53 +18,79 @@ const Navbar = () => {
   )
 
   return (
-    <div className='bg-[#181918]'>
-      <div className='w-[100%] lg:w-[1130px] mx-auto h-[14vh] border-b border-[#3a3a3a] flex justify-between items-center'>
+    <div>
+      {/* FOR LARGE DEVICE  */}
+      <div className='bg-[#181918] hidden lg:block'>
+        <div className='w-[100%] lg:w-[1130px] mx-auto h-[14vh] border-b border-[#3a3a3a] flex justify-between items-center'>
+          <div className='logo'>
+            <img src={logo} alt='Logo' />
+          </div>
+
+          <div className='links flex gap-10 items-center text-[15px] text-white font-semibold uppercase tracking-widest'>
+            <NavLink
+              to={'/'}
+              className={({ isActive }) =>
+                `cursor-pointer transition-all duration-300 delay-75 hover:text-[#d87d4a] ${
+                  isActive ? 'text-[#d87d4a]' : 'text-white'
+                }`
+              }
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to={'/headphones'}
+              className={({ isActive }) =>
+                `cursor-pointer transition-all duration-300 delay-75 hover:text-[#d87d4a] ${
+                  isActive ? 'text-[#d87d4a]' : 'text-white'
+                }`
+              }
+            >
+              Headphones
+            </NavLink>
+            <NavLink
+              to={'/speakers'}
+              className={({ isActive }) =>
+                `cursor-pointer transition-all duration-300 delay-75 hover:text-[#d87d4a] ${
+                  isActive ? 'text-[#d87d4a]' : 'text-white'
+                }`
+              }
+            >
+              Speakers
+            </NavLink>
+            <NavLink
+              to={'/earphones'}
+              className={({ isActive }) =>
+                `cursor-pointer transition-all duration-300 delay-75 hover:text-[#d87d4a] ${
+                  isActive ? 'text-[#d87d4a]' : 'text-white'
+                }`
+              }
+            >
+              Earphones
+            </NavLink>
+          </div>
+
+          <div className='cart relative' onClick={toggleModal}>
+            <img className='cursor-pointer' src={cartIcon} alt='Cart Icon' />
+            <div className='w-[19px] h-[19px] bg-[#d87d4a] rounded-full font-bold flex justify-center items-center absolute -top-3 -right-3'>
+              <p className='text-[13px] text-white'>{totalQuantity}</p>
+            </div>
+          </div>
+
+          {isModalOpen && (
+            <Modal
+              cartItems={cartItems}
+              setCartItems={setCartItems}
+              onClose={toggleModal}
+            />
+          )}
+        </div>
+      </div>
+
+      {/* FOR MOBILE DEVICES  */}
+      <div className='bg-[#181918] flex items-center justify-between lg:hidden h-[14vh] px-6'>
+        <GiHamburgerMenu className='text-[25px] text-white font-bold' />
         <div className='logo'>
           <img src={logo} alt='Logo' />
-        </div>
-
-        <div className='links flex gap-10 items-center text-[15px] text-white font-semibold uppercase tracking-widest'>
-          <NavLink
-            to={'/'}
-            className={({ isActive }) =>
-              `cursor-pointer transition-all duration-300 delay-75 hover:text-[#d87d4a] ${
-                isActive ? 'text-[#d87d4a]' : 'text-white'
-              }`
-            }
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to={'/headphones'}
-            className={({ isActive }) =>
-              `cursor-pointer transition-all duration-300 delay-75 hover:text-[#d87d4a] ${
-                isActive ? 'text-[#d87d4a]' : 'text-white'
-              }`
-            }
-          >
-            Headphones
-          </NavLink>
-          <NavLink
-            to={'/speakers'}
-            className={({ isActive }) =>
-              `cursor-pointer transition-all duration-300 delay-75 hover:text-[#d87d4a] ${
-                isActive ? 'text-[#d87d4a]' : 'text-white'
-              }`
-            }
-          >
-            Speakers
-          </NavLink>
-          <NavLink
-            to={'/earphones'}
-            className={({ isActive }) =>
-              `cursor-pointer transition-all duration-300 delay-75 hover:text-[#d87d4a] ${
-                isActive ? 'text-[#d87d4a]' : 'text-white'
-              }`
-            }
-          >
-            Earphones
-          </NavLink>
         </div>
 
         <div className='cart relative' onClick={toggleModal}>
