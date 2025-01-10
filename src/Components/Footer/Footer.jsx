209cab6b878b1602f82434f13daeb788bea196d5
SajeedAninda <sajaninda@gmail.com>
2025-01-10 17:54:41 +0600

    Made footer responsive

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,11 +5,11 @@ import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div className='bg-[#191919]'>
-      <div className='w-[100%] lg:w-[1130px] mx-auto py-20 flex justify-between gap-20'>
-        <div className='flex-1'>
-          <img className='' src={logo} alt='' />
-          <p className='mt-12 text-[15px] text-[#ffffff80]'>
+    <div className='bg-[#191919] px-6 lg:px-0'>
+      <div className='w-[100%] lg:w-[1130px] mx-auto py-20 flex flex-col lg:flex-row justify-between gap-8 lg:gap-20'>
+        <div className='flex-1 flex flex-col items-center lg:items-start lg:text-left'>
+          <img className='w-[150px]' src={logo} alt='' />
+          <p className='mt-12 text-[15px] text-[#ffffff80] text-center lg:text-left'>
             Audiophile is an all in one stop to fulfill your audio needs. We're
             a small team of music lovers and sound specialists who are devoted
             to helping you get the most out of personal audio. Come and visit
@@ -20,8 +20,8 @@ const Footer = () => {
           </p>
         </div>
 
-        <div className='flex-1 flex flex-col justify-between'>
-          <div className='links flex gap-10 items-center justify-end text-[13px] text-white font-semibold uppercase tracking-widest'>
+        <div className='flex-1 flex flex-col items-center lg:items-end gap-6 lg:gap-0 justify-between'>
+          <div className='links flex flex-col lg:flex-row gap-4 lg:gap-10 items-center justify-end text-[13px] text-white font-semibold uppercase tracking-widest'>
             <Link
               to={'/'}
               className='hover:text-[#d87d4a] cursor-pointer transition-all duration-300 delay-75'
